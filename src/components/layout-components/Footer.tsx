9e5bfc277ef1f927ac0c8afb2be94bf91bb6dd0a
saigyou_ayakashi <minuit1993@live.fr>
2022-11-21 17:49:06 +0100

    add time for left santa and check day for open modal

--- src/components/layout-components/Footer.tsx
+++ src/components/layout-components/Footer.tsx
@@ -1,9 +1,13 @@
+import moment from "moment-timezone";
+
 export default function Footer() {
+  const year = moment().tz("Europe/Paris").format("YYYY");
+  const daysLeft = moment(`${year}-12-25`).diff(moment(), "days");
+
   return (
     <footer>
-      <p>
-        Made with <span>❤️</span> by Ydainna
-      </p>
+      <p>Made with <span>❤️</span> by Ydainna</p>
+      <p>Il reste {daysLeft} jours avant Noël</p>
     </footer>
   )
 }
